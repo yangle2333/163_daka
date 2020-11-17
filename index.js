899e503fca2ff7ac5902e560d081a209deab123c
yangle2333 <74586440+yangle2333@users.noreply.github.com>
2020-11-17 14:36:46 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // 用户名和密码
-const phone = '';
-const password = '';
+const phone = '13171697521';
+const password = '0106221987le';
 
 const rp = require('request-promise');
 const tough = require('tough-cookie');
